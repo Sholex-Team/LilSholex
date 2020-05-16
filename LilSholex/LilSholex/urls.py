e426cdb13a019b7b2cfde7bd815029129bbf4369
Sholex Team <realantiwanted@gmail.com>
2020-05-16 09:40:15 +0430

    Adding new health check for gunicorn

--- LilSholex/LilSholex/urls.py
+++ LilSholex/LilSholex/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from LilSholex import views
 
-urlpatterns = [
+urlpatterns = (
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('groupguard/', include('groupguard.urls')),
     path('support/', include('support.urls')),
     path('persianmeme/', include('persianmeme.urls')),
     path('anonymous/', include('anonymous.urls'))
-]
+)
